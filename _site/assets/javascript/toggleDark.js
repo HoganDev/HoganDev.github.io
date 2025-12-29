0cc01a35cd79c0f40428180bd1687a6ba4a71793
Matt Hogan <hogandev@gmail.com>
2025-12-29 10:10:16 -0500

    Refactor layout and styles; enhance header animation and dark mode toggle
    - Updated grid layout in _base.scss to include additional columns for better responsiveness.
    - Enhanced header styles in header.scss, adding a flower icon with animation and adjusting the myname class for improved typography.
    - Modified main.css to reflect changes in grid layout and updated styles for the header.
    - Updated main.css.map to match the new styles and layout.
    - Added a random animation effect to the myname element in toggleDark.js, wrapping each letter in a span for individual animation.
    - Changed the header title in index.html to use the new myname class.
    - Cleaned up unused sections in index.html for better readability.
    - Added CNAME file for custom domain setup.

--- _site/assets/javascript/toggleDark.js
+++ _site/assets/javascript/toggleDark.js
@@ -10,3 +10,28 @@ checkbox.addEventListener('change', function() {
         moon.innerHTML = feather.icons['moon'].toSvg({'fill': 'var(--fg-color-dark)'})
     }
 })
+
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.myname');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.myname .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.myname',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 10000 * randomIntFromInterval(1, 1000)
+  });
